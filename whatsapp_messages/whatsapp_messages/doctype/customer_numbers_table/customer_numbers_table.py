849d5d9af22c87d9d4eff168c92e00aa84208452
abdopcnet <abdopcnet@gmail.com>
2025-11-09 21:25:07 +0300

    Add customer_numbers_table.py file to define the customer_numbers_table document in the Whatsapp Messages module.

---
+++ whatsapp_messages/whatsapp_messages/doctype/customer_numbers_table/customer_numbers_table.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, abdopcnet@gmail.com and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class customer_numbers_table(Document):
+	pass
