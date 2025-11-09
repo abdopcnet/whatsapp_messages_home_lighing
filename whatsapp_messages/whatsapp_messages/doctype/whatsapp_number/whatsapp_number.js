82d24e23a727c711426e7c7230ad14d4a31979da
abdopcnet <abdopcnet@gmail.com>
2025-11-09 21:25:38 +0300

    Add reload button to whatsapp_number form for refreshing data

--- whatsapp_messages/whatsapp_messages/doctype/whatsapp_number/whatsapp_number.js
+++ whatsapp_messages/whatsapp_messages/doctype/whatsapp_number/whatsapp_number.js
@@ -1,8 +1,11 @@
 // Copyright (c) 2025, abdopcnet@gmail.com and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("whatsapp_number", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("whatsapp_number", {
+	refresh(frm) {
+		// Add reload button directly in toolbar
+		frm.add_custom_button(__('تحديث'), function() {
+			frm.reload_doc();
+		});
+	}
+});
