c5e0d0b4254ea12b9a17292f0296550bf2c54e17
abdopcnet <abdopcnet@gmail.com>
2025-11-09 21:25:34 +0300

    Add test_fetch_customer_numbers.py to implement unit tests for customer number fetching functionality in the Whatsapp Messages module.

---
+++ whatsapp_messages/whatsapp_messages/doctype/fetch_customer_numbers/test_fetch_customer_numbers.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, abdopcnet@gmail.com and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class Testfetch_customer_numbers(FrappeTestCase):
+	pass
